8fd2f62562384f38d18a8fc04e66e26681967eba
Amouret <amouretcosmos@gmail.com>
2026-07-14 22:05:33 +0100

    Add files via upload

---
+++ Works.jsx
@@ -0,0 +1,18 @@
+function Works() {
+  return (
+    <section
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h2>
+        WORKS TEST
+      </h2>
+    </section>
+  );
+}
+
+export default Works;
